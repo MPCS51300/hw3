0b58bf23c996b9a19621525b3640586f9fb99c9f
Linxuan Xu <linxuanseph@gmail.com>
2019-11-13 12:45:47 -0600

    add print and generate_stmt

--- codeGen.py
+++ codeGen.py
@@ -4,6 +4,42 @@ import yaml
 variables = {} # track variables: key=current_function_variable_name, value:llvmlite_type
 current_func_prefix = None # track the current funtion
 
+#read print and printslit functions' IR
+with open("print.ll", "r") as input:  
+    llvm_ir = input.read()
+
+def create_execution_engine():
+    """
+    Create an ExecutionEngine suitable for JIT code generation on
+    the host CPU.  The engine is reusable for an arbitrary number of
+    modules.
+    """
+    # Create a target machine representing the host
+    target = llvm.Target.from_default_triple()
+    target_machine = target.create_target_machine()
+    # And an execution engine with an empty backing module
+    backing_mod = llvm.parse_assembly("")
+    engine = llvm.create_mcjit_compiler(backing_mod, target_machine)
+    return engine
+
+
+def compile_ir(engine, llvm_ir):
+    """
+    Compile the LLVM IR string with the given engine.
+    The compiled module object is returned.
+    """
+    # Create a LLVM module object from the IR
+    mod = llvm.parse_assembly(llvm_ir)
+    mod.verify()
+    engine.add_module(mod)
+    engine.finalize_object()
+    engine.run_static_constructors()
+    return mod
+
+#generate print function codes
+engine = create_execution_engine()
+print_module = compile_ir(engine, llvm_ir)
+
 def generate_type(type):
     if type == "int":
         return ir.IntType(32)
@@ -20,6 +56,11 @@ def generate_type(type):
             return ir.PointerType(ir.FloatType())
         elif type == "bool":
             return ir.PointerType(ir.IntType(1))
+    elif type == "slit":
+        return ir.PointerType(ir.IntType(8))
+
+def generate_slit(string):
+    return ir.Constant(generate_type("slit"), string)
 
 def generate_extern(ast, module):
     args = []
@@ -131,29 +172,59 @@ def generate_exp(ast, module, builder):
     elif name == "funccall":
         return generate_funccall(ast, module, builder)
 
-def generate_stmt(ast, module, builder):
+def generate_stmt(ast, module, builder, func):
     name = ast["name"]
-    if name == "if":
+    if name == "blk":
+        generate_blk(ast, module, builder)
+    elif name == "if":
         pred = generate_exp(ast["cond"], module, builder)
-        res = builder.if_then(pred)
-        with builder.if_then(pred) as (then):
-            # with then:
-            #     print(then)
-            #     
-            generate_stmt(ast["stmt"], module, builder)
+        if "else_stmt" in ast:
+            with builder.if_else(pred) as (then, otherwise):
+                with then:
+                    generate_stmt(ast["stmt"], module, builder)
+                with otherwise:
+                    generate_stmt(ast["else_stmt"], module, builder)
+        else:
+            with builder.if_then(pred) as (then):
+                with then:
+                    generate_stmt(ast["stmt"], module, builder)
+    elif name == "ret":
+        if "exp" in ast:
+            generate_exp(ast["exp"], module, builder)
+    elif name == "vardeclstmt":  
+        var = builder.alloca(generate_type(ast["vdecl"]["type"], name = ast["vdecl"]["var"]))
+        builder.store(generate_exp(ast["exp"], module, builder), var)
+    elif name == "expstmt":
+        generate_exp(ast["exp"], module, builder)
+    elif name == "while":
+        loop_head = func.append_basic_block("loop.header")
+        loop_body = func.append_basic_block("loop.body")
+        loop_end = func.append_basic_block("loop.end")
+        builder.branch(loop_head)
+        builder.position_at_end(loop_head)
+        cond = generate_exp(ast["cond"], module, builder)
+        builder.cbranch(cond, loop_body, loop_end)
+        builder.position_at_end(loopbody)
+        #loop body
+        generate_stmt(ast["stmt"], module, builder, func)
+        #jump to loop head
+        builder.branch(loop_head)
+        builder.position_at_end(loop_end)
+    elif name == "print":
+        args = []
+        args.append(generate_exp(ast["exp"], module, builder))
+        fn = print_module.get_global("print")
+        builder.call(fn, args)
+    elif name == "printslit":
+        args = []
+        args.append(generate_slit(ast["string"]))
+        fn = print_module.get_global("printslit")
+        builder.call(fn, args)
 
-        
-    # elif name == "ret":
-    # elif name == "vardeclstmt":
-    # elif name == "expstmt":
-    # elif name == "while":
-    # elif name == "print":
-    # elif name == "printslit":
-
-def generate_blk(ast, module, builder):
+def generate_blk(ast, module, builder, func):
     if "contents" in ast:
         for stmt in ast["contents"]["stmts"]:
-            generate_stmt(stmt, module, builder)
+            generate_stmt(stmt, module, builder, func)
 
 def generate_func(ast, module):
     global current_func_prefix
@@ -172,9 +243,9 @@ def generate_func(ast, module):
     # Now implement the function
     entry_block = func.append_basic_block(name="entry")
     builder = ir.IRBuilder(entry_block)
-
+   
     if "blk" in ast:
-        result = generate_blk(ast["blk"], module, builder)
+        result = generate_blk(ast["blk"], module, builder, func)
     
     builder.ret(result)
 
@@ -194,6 +265,7 @@ def convert(ast, module):
 
 file = open("test_files/test1_ekcc.yml", "r") 
 ast = yaml.load(file.read(), Loader=yaml.FullLoader)
+
 module = ir.Module(name="prog")
 convert(ast, module)
 print(module)
